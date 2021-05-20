baba502d469d43792b54d0293e514428cc83ec6c
Olly Butters <OllyButters@users.noreply.github.com>
2021-05-20 12:36:12 +0100

    Update timestamp.js

--- timestamp.js
+++ timestamp.js
@@ -1,5 +1,5 @@
 function update_timestamp() {
-    fetch("https://ollybutters.github.io/puma/timestamp.html")
+    fetch("/timestamp.html")
     .then(response => {
         return response.text()
     })
