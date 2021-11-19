4663411eaf0ab98f9cc46a433aba584e9d7be234
OllyButters <olly.butters@newcastle.ac.uk>
2021-11-19 14:36:17 +0000

    updated for new citations

--- keywords/Aged/stats.js
+++ keywords/Aged/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2020',7],['2021',6],]);var citations =([['Year', 'Number of Citations'],['2020',58],['2021',69],]);
+var papers =([['Year', 'Number of papers'],['2020',7],['2021',7],]);var citations =([['Year', 'Number of Citations'],['2020',58],['2021',69],]);
--- keywords/Humans/stats.js
+++ keywords/Humans/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2019',1],['2020',33],['2021',22],]);var citations =([['Year', 'Number of Citations'],['2019',2],['2020',187],['2021',78],]);
+var papers =([['Year', 'Number of papers'],['2019',1],['2020',33],['2021',23],]);var citations =([['Year', 'Number of Citations'],['2019',2],['2020',187],['2021',78],]);
