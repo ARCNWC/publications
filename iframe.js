e644f0678fb057fda35dd48bcad515713350714b
Olly Butters <OllyButters@users.noreply.github.com>
2021-12-09 17:24:18 +0000

    loop over resize

--- iframe.js
+++ iframe.js
@@ -4,12 +4,22 @@ if ( window.location !== window.parent.location )
     // The page is in an iFrames
     console.log("The page is in an iFrame");
     
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+     }
+
     // Add a listener to post to parent window the size of the screen. This is used
     // when displayed in an iframe and needs the dimensions to resize
     window.addEventListener('load', function() {
         //let message = { height: document.body.scrollHeight, width: document.body.scrollWidth };	
     
         if ( window.location !== window.parent.location ) {        
+
+        // hide the header
+        var elem = document.querySelector('#header-container');
+        elem.style.display = 'none';
+
+        for (let i = 0; i < 5; i++) { 
 
         content_height = document.getElementById("wrapper").scrollHeight;
         footer_height = document.getElementsByClassName("foot")[0].scrollHeight;
@@ -18,11 +28,12 @@ if ( window.location !== window.parent.location )
 
         // window.top refers to parent window
         window.top.postMessage(message, "*");
-        console.log(message)
+        console.log(message);
 
-        // hide the header
-        var elem = document.querySelector('#header-container');
-        elem.style.display = 'none';
+        await(sleep(1000));
+
+        }
+
 
         } else {
             console.log("Spurious onload")
