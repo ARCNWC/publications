8aa59f0005695ed3e04dc6a6e699c04e1d5492b9
OllyButters <olly.butters@newcastle.ac.uk>
2021-12-17 09:17:08 +0000

    updated midas

--- tags/MIDAS/stats.js
+++ tags/MIDAS/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2019',1],['2020',30],['2021',24],]);var citations =([['Year', 'Number of Citations'],['2019',2],['2020',113],['2021',5],]);
+var papers =([['Year', 'Number of papers'],['2019',1],['2020',29],['2021',24],]);var citations =([['Year', 'Number of Citations'],['2019',2],['2020',111],['2021',5],]);
