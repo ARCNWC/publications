94e85a81188b026df3127a7f1530e3d7a4f5eb14
OllyButters <oliver.butters@gmail.com>
2024-06-18 16:50:42 +0100

    stats update

--- keywords/Commentary/stats.js
+++ keywords/Commentary/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2022',2],['2023',3],]);var citations =([['Year', 'Number of Citations'],['2022',1],['2023',1],]);
+var papers =([['Year', 'Number of papers'],['2022',2],['2023',3],['2024',1],]);var citations =([['Year', 'Number of Citations'],['2022',1],['2023',1],['2024',0],]);
