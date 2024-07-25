24d6b1182844de04f36653702293a95d20bd1098
OllyButters <oliver.butters@gmail.com>
2024-07-25 21:49:20 +0100

    stats update

--- keywords/Islam/stats.js
+++ keywords/Islam/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2019',2],['2020',2],['2021',2],]);var citations =([['Year', 'Number of Citations'],['2019',36],['2020',32],['2021',39],]);
+var papers =([['Year', 'Number of papers'],['2019',2],['2020',2],['2021',2],]);var citations =([['Year', 'Number of Citations'],['2019',36],['2020',32],['2021',40],]);
